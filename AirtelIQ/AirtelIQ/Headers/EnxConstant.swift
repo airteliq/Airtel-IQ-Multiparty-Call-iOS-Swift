b5d341e5991ecc3af57ae6ebcf795a8c4c5b9780
rajeshg-vcloudx <rajesh.g@vcloudx.com>
2021-08-23 18:48:05 +0530

    Airtel Video IQ - Sample App - Initial Commit

---
+++ AirtelIQ/AirtelIQ/Headers/EnxConstant.swift
@@ -0,0 +1,23 @@
+//
+//  EnxConstant.swift
+//  QuickApp_iOS
+//
+//  Created by Daljeet Singh on 04/12/18.
+//  Copyright © 2018 Daljeet Singh. All rights reserved.
+//
+
+import Foundation
+
+    /* To try the app with Enablex hosted service you need to set the kTry = true */
+        let kTry      = true
+
+    /*Your webservice host URL, Keet the defined host when kTry = true */
+        let kBasedURL = "https://api.videoiq.airtel.in/"
+
+    
+    /*The following information required, Only when kTry = true, When you hosted your own webservice remove these fileds*/
+
+    /*Use enablec portal to create your app and get these following credentials*/
+        let kAppId    = "AppID"
+        let kAppkey   = "AppKey"
+
