33609c2f65d8f33390737bfdc2e1ab20dd33a26c
jaykumarvcx <57702957+jaykumarvcx@users.noreply.github.com>
2022-02-14 19:16:42 +0530

    Fixed Rest Apis issue

--- AirtelIQ/AirtelIQ/Headers/EnxConstant.swift
+++ AirtelIQ/AirtelIQ/Headers/EnxConstant.swift
@@ -18,6 +18,6 @@ import Foundation
     /*The following information required, Only when kTry = true, When you hosted your own webservice remove these fileds*/
 
     /*Use enablec portal to create your app and get these following credentials*/
-        let kAppId    = "6209ebe32d088e7c94313f24"
-        let kAppkey   = "buPySy9aVybevaUuaaWyHyryRuQaquRuqy9y"
+        let kAppId    = "App_id"
+        let kAppkey   = "App_key"
 
